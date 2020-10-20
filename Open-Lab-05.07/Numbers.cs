61554850978e9d61bf7e17c7cb704cc641eec835
Jaro32768 <jarko04@hotmail.sk>
2020-10-20 13:38:17 +0200

    vypracovanie

--- Open-Lab-05.07/Numbers.cs
+++ Open-Lab-05.07/Numbers.cs
@@ -6,7 +6,29 @@ namespace Open_Lab_05._07
     {
         public int[] RemoveSmallest(int[] nums)
         {
-            throw new NotImplementedException();
+            int[] ret = new int[nums.Length - 1];
+            int s = 0;
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[s])
+                {
+                    s = i;
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == s)
+                {
+
+                }
+                else
+                {
+                    ret[k] = nums[i];
+                    k++;
+                }
+            }
+            return ret;
         }
     }
 }
